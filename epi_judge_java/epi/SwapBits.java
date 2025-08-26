023001916a5de862cc0aff34d40ef7c5125e5fa4
Linton Charles <lintoncharles2000@gmail.com>
2025-08-26 10:21:08 +1000

    Solve 4.2 Swap Bits

--- epi_judge_java/epi/SwapBits.java
+++ epi_judge_java/epi/SwapBits.java
@@ -4,8 +4,20 @@ import epi.test_framework.GenericTest;
 public class SwapBits {
   @EpiTest(testDataFile = "swap_bits.tsv")
   public static long swapBits(long x, int i, int j) {
-    // TODO - you fill in here.
-    return 0;
+    long xi = x & (long) 1 << i;  // Masking with 2^i to get ith bit
+    long xj = x & (long) 1 << j;
+
+    if (xi == 0 && xj != 0){
+      long val_i = (long) 1 << i;
+      return x - xj + val_i;
+    } 
+    else if (xi != 0 && xj == 0){
+      long val_j = (long) 1 << j;
+      return x + val_j - xi;
+    }
+    else {
+      return x;
+    }
   }
 
   public static void main(String[] args) {
