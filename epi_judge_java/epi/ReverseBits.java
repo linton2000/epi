a7f1c29278766ca3fb9dfc29cb6ba576d4d86d45
Linton Charles <lintoncharles2000@gmail.com>
2025-08-26 11:17:32 +1000

    4.3 Reverse Bits - Add brute force solution

--- epi_judge_java/epi/ReverseBits.java
+++ epi_judge_java/epi/ReverseBits.java
@@ -4,8 +4,19 @@ import epi.test_framework.GenericTest;
 public class ReverseBits {
   @EpiTest(testDataFile = "reverse_bits.tsv")
   public static long reverseBits(long x) {
-    // TODO - you fill in here.
-    return 0;
+    // Brute Force
+    long y = 0;  // Reversed x
+    long i = 63;  // Bit index starting from MSB
+
+    while (x > 0){
+      if ((x & 1) == 1){
+        y += 1L << i;
+      }
+      x >>>= 1;
+      i--;
+    }
+
+    return y;
   }
 
   public static void main(String[] args) {
