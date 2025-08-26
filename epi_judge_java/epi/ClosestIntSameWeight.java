3ca8399622be9007929faf91c5f3fa98532d3cc8
Linton Charles <lintoncharles2000@gmail.com>
2025-08-26 14:38:44 +1000

    4.4 Closest Int Same Weight - Add brute force

--- epi_judge_java/epi/ClosestIntSameWeight.java
+++ epi_judge_java/epi/ClosestIntSameWeight.java
@@ -4,11 +4,29 @@ import epi.test_framework.GenericTest;
 public class ClosestIntSameWeight {
   @EpiTest(testDataFile = "closest_int_same_weight.tsv")
   public static long closestIntSameBitCount(long x) {
-    // TODO - you fill in here.
-    return 0;
+    // Brute Force
+    long d = 1;
+    int targetWeight = getWeight(x);
+    while (true) {
+      if (getWeight(x - d) == targetWeight)
+        return x - d;
+      if (getWeight(x + d) == targetWeight)
+        return x + d;
+      d++;
+    }
+  }
+
+  public static int getWeight(long x){
+    int weight = 0;
+    while (x > 0){
+      x &= x - 1;  // Remove least sig. 1 bit (called Brian Kernighan’s trick)
+      weight++;
+    }
+    return weight;
   }
 
   public static void main(String[] args) {
+    //System.out.println(getWeight(0));
     System.exit(
         GenericTest
             .runFromAnnotations(args, "ClosestIntSameWeight.java",
--- problem_mapping.js
+++ problem_mapping.js
@@ -62,7 +62,7 @@ problem_mapping = {
                 "total": 10000
             },
             "Java: ClosestIntSameWeight.java": {
-                "passed": 0,
+                "passed": 10000,
                 "total": 10000
             },
             "Python: closest_int_same_weight.py": {
