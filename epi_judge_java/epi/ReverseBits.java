008fcbfaccb774e8e9e71fa7b03493e4ca7e5535
Linton Charles <lintoncharles2000@gmail.com>
2025-08-26 13:06:13 +1000

    4.3 Reverse Bits - Add half iteration solution

--- epi_judge_java/epi/ReverseBits.java
+++ epi_judge_java/epi/ReverseBits.java
@@ -1,11 +1,14 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+
 public class ReverseBits {
   @EpiTest(testDataFile = "reverse_bits.tsv")
   public static long reverseBits(long x) {
     // Brute Force
-    long y = 0;  // Reversed x
+    // Time Complexity: O(n) where n = no. of bits in x
+/*     long y = 0;  // Reversed x
     long i = 63;  // Bit index starting from MSB
 
     while (x > 0){
@@ -16,7 +19,22 @@ public class ReverseBits {
       i--;
     }
 
-    return y;
+    return y; */
+
+    // Better Sol 1
+    // Same time complexity but 1/2 constant
+    long lo = 0;
+    long hi = 63;
+
+    while (lo < hi){
+      if(((x >>> hi) & 1L) != ((x >>> lo) & 1L)){
+        long bitMask = (1L << hi) | (1L << lo);
+        x ^= bitMask;
+      }
+      lo++;
+      hi--;
+    }
+    return x;
   }
 
   public static void main(String[] args) {
