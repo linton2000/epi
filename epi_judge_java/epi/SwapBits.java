e98fb1ffa853a9aaa1bb475a0acdafb9d886ba7f
Linton Charles <lintoncharles2000@gmail.com>
2025-08-26 10:36:42 +1000

    Add textbook solution for 4.2

--- epi_judge_java/epi/SwapBits.java
+++ epi_judge_java/epi/SwapBits.java
@@ -4,7 +4,9 @@ import epi.test_framework.GenericTest;
 public class SwapBits {
   @EpiTest(testDataFile = "swap_bits.tsv")
   public static long swapBits(long x, int i, int j) {
-    long xi = x & (long) 1 << i;  // Masking with 2^i to get ith bit
+
+    // Brute Force (same time complexity but less succinct/efficient than textbook sol)
+/*     long xi = x & (long) 1 << i;  // Masking with 2^i to get ith bit
     long xj = x & (long) 1 << j;
 
     if (xi == 0 && xj != 0){
@@ -17,7 +19,14 @@ public class SwapBits {
     }
     else {
       return x;
+    } */
+
+    // Textbook sol (if bits differ, toggle them with XOR using bitmask)
+    if ((x >>> i & 1) != (x >>> j & 1)){     // Isolate 0/1 bits at i & j to compare
+      long bitMask = (1L << i) | (1L << j);  // Create long var with i & j bits at appropriate positions
+      x ^= bitMask;                          // Toggle x's bits at i & j by XORing with bit mask (0 becomes 1 & vice versa)
     }
+    return x;
   }
 
   public static void main(String[] args) {
