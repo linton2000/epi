4bd650b940d1e19b1616a01a3d633c30fbace8cf
Linton Charles <lintoncharles2000@gmail.com>
2025-09-05 17:58:55 +1000

    4.8 Reverse Digits - Add buggy initial attempt

--- epi_judge_java/epi/IsNumberPalindromic.java
+++ epi_judge_java/epi/IsNumberPalindromic.java
@@ -4,15 +4,37 @@ import epi.test_framework.GenericTest;
 public class IsNumberPalindromic {
   @EpiTest(testDataFile = "is_number_palindromic.tsv")
   public static boolean isPalindromeNumber(int x) {
-    // TODO - you fill in here.
-    return true;
+    // Initial Attempt - Repeated div by 10 & reverse digits, i.e. 1331: 1331/100 == rev(31)?
+    if (x < 0) return false;  // The sign makes it non-palindromic
+    if (x < 10) return true;  // All single digits are palindromes
+
+    int i = 0;
+    int mid = ((int) Math.log10(x) + 1) / 2;  // Index of int midpoint
+    int rev = 0;
+
+    while (i < mid) {
+      rev = (rev * 10) + (x % 10);
+      x /= 10;
+      i++;
+    }
+      
+    if (hasEvenDigits(x) != hasEvenDigits(rev))  // Shave off the odd middle digit
+      x /= 10;
+    
+    return x == rev;
+  }
+
+  public static boolean hasEvenDigits(int x){
+    if (x < 10) return false;
+    return (((int) (Math.log10(x)) + 1) % 2) == 0;
   }
 
   public static void main(String[] args) {
-    System.exit(
+    System.out.println(isPalindromeNumber(1810));
+/*     System.exit(
         GenericTest
             .runFromAnnotations(args, "IsNumberPalindromic.java",
                                 new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+            .ordinal()); */
   }
 }
