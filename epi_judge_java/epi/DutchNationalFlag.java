70d29b7504bbcf2d714840989184a1a1505af826
Linton Charles <lintoncharles2000@gmail.com>
2025-09-12 16:21:11 +1000

    5.1 DNF Partitioning - Add Brute Force sol

--- epi_judge_java/epi/DutchNationalFlag.java
+++ epi_judge_java/epi/DutchNationalFlag.java
@@ -10,7 +10,28 @@ public class DutchNationalFlag {
   public enum Color { RED, WHITE, BLUE }
 
   public static void dutchFlagPartition(int pivotIndex, List<Color> A) {
-    // TODO - you fill in here.
+    // Initial Attempt - Brute Force
+    // Space: O(n), Time: O(n^2)
+/*     List<Color> res = new ArrayList<>();
+    int lp = 0; // Less than Pivot ptr (end of 1st subarray)
+    int p = 0;  // Equal to Pivot ptr (end of 2nd subaray)
+
+    for (int i = 0; i < A.size(); i++) {
+      if (A.get(i) == Color.RED) {
+        res.add(lp, A.get(i));
+        lp++;
+        p++;
+      } 
+      else if (A.get(i) == Color.WHITE) {
+        res.add(p, A.get(i));
+        p++;
+      }
+      else {  // BLUE color
+        res.add(A.get(i));
+      }
+    }
+    A.clear();
+    A.addAll(res);  // Deep copy into A */
     return;
   }
   @EpiTest(testDataFile = "dutch_national_flag.tsv")
