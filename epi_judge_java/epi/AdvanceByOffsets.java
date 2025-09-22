6c7c1ff3060b29c900c41d01a9e1da33fbd9b2a8
Linton Charles <lintoncharles2000@gmail.com>
2025-09-22 17:01:22 +1000

    5.4 Adv. by offsets - Add buggy initial attempt

--- epi_judge_java/epi/AdvanceByOffsets.java
+++ epi_judge_java/epi/AdvanceByOffsets.java
@@ -6,8 +6,16 @@ import java.util.List;
 public class AdvanceByOffsets {
   @EpiTest(testDataFile = "advance_by_offsets.tsv")
   public static boolean canReachEnd(List<Integer> maxAdvanceSteps) {
-    // TODO - you fill in here.
-    return true;
+    // Initial Attempt
+    if (maxAdvanceSteps.size() == 1)
+      return true;
+
+    boolean res = false;
+    for (int i = maxAdvanceSteps.get(0); i > 0; i--) {
+      res |= canReachEnd(maxAdvanceSteps.subList(i, maxAdvanceSteps.size()));
+      if (res) return res;
+    }
+    return res;
   }
 
   public static void main(String[] args) {
