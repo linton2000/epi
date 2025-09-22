ae00a1e244cd716f634a532ea10b83cf8075d338
Linton Charles <lintoncharles2000@gmail.com>
2025-09-22 19:00:24 +1000

    5.5 Delete Duplicates - Add bad initial attempt

--- epi_judge_java/epi/SortedArrayRemoveDups.java
+++ epi_judge_java/epi/SortedArrayRemoveDups.java
@@ -3,12 +3,28 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
 public class SortedArrayRemoveDups {
   // Returns the number of valid entries after deletion.
   public static int deleteDuplicates(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
+    // Initial Attempt
+/*     for (int i = 0; i < (A.size() - 1); i++) {
+      int j = i;
+      while (A.get(j) == A.get(j + 1))
+        j++;
+
+      // Shuffle left & overwrite duplicates
+      int k = 1;
+      while (k <= (j - i)) {
+        A.set(i + k, A.get(j + k));
+        k++;
+      }
+    }
+
+    return A.size(); */
   }
   @EpiTest(testDataFile = "sorted_array_remove_dups.tsv")
   public static List<Integer> deleteDuplicatesWrapper(TimedExecutor executor,
@@ -19,10 +35,14 @@ public class SortedArrayRemoveDups {
   }
 
   public static void main(String[] args) {
-    System.exit(
+    List<Integer> A = new ArrayList<>(Arrays.asList(2, 3, 5, 5, 7, 11, 11, 11, 13));
+    deleteDuplicates(A);
+    System.out.println(A);
+
+/*     System.exit(
         GenericTest
             .runFromAnnotations(args, "SortedArrayRemoveDups.java",
                                 new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+            .ordinal()); */
   }
 }
