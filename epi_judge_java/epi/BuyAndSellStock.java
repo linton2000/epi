35367c4fa8d4b6e30e464b3a3cf80ab612fa4714
Linton Charles <lintoncharles2000@gmail.com>
2025-09-24 13:08:48 +1000

    5.6 Buy Sell Once - Add brute force sol

--- epi_judge_java/epi/BuyAndSellStock.java
+++ epi_judge_java/epi/BuyAndSellStock.java
@@ -6,8 +6,18 @@ import java.util.List;
 public class BuyAndSellStock {
   @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
   public static double computeMaxProfit(List<Double> prices) {
-    // TODO - you fill in here.
-    return 0.0;
+    // Initial Attempt - Brute force optimisation
+    // Time: O(n^2), Space: O(1)
+    // Invariant: p is max profit for all prices 0...i, at end of iteration
+    Double p = 0.0;
+
+    for (int i = 0; i < prices.size(); i++) {
+      for (int j = (i + 1); j < prices.size(); j++) {
+        if ((prices.get(j) - prices.get(i)) > p)
+          p = prices.get(j) - prices.get(i);
+      }
+    }
+    return p;
   }
 
   public static void main(String[] args) {
