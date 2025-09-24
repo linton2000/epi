d8af67cc681145876a09150bf93ae65ba1d6c025
Linton Charles <lintoncharles2000@gmail.com>
2025-09-24 13:24:43 +1000

    5.6 Buy Sell Once - Add optimal sol

--- epi_judge_java/epi/BuyAndSellStock.java
+++ epi_judge_java/epi/BuyAndSellStock.java
@@ -9,7 +9,7 @@ public class BuyAndSellStock {
     // Initial Attempt - Brute force optimisation
     // Time: O(n^2), Space: O(1)
     // Invariant: p is max profit for all prices 0...i, at end of iteration
-    Double p = 0.0;
+/*     Double p = 0.0;
 
     for (int i = 0; i < prices.size(); i++) {
       for (int j = (i + 1); j < prices.size(); j++) {
@@ -17,7 +17,23 @@ public class BuyAndSellStock {
           p = prices.get(j) - prices.get(i);
       }
     }
-    return p;
+    return p; */
+
+    // 2nd Attempt - One pass w. running max
+    // Time: O(n), Space: O(1)
+    // Invariant: Min. stock prices & max profit of prices[0:i] are always in `cheapest` & `maxP`
+    Double maxP = 0.0;
+    Double cheapest = prices.get(0);
+
+    for (int i = 1; i < prices.size(); i++) {
+      if ((prices.get(i) - cheapest) > maxP)
+        maxP = prices.get(i) - cheapest;
+      
+      if (prices.get(i) < cheapest)
+        cheapest = prices.get(i);
+    }
+
+    return maxP;
   }
 
   public static void main(String[] args) {
